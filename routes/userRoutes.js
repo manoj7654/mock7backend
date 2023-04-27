32d2df9e5da1a61466c9e8c5aed1ebb3834d7ce2
Manoj Kumar <manojsfstm5@gmail.com>
2023-04-27 15:30:09 +0530

    backend file created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,8 +25,8 @@ userRouter.get("/",(req,res)=>{
     res.send("hello from users")
 })
 
+
 userRouter.get("/allusers",async(req,res)=>{
-   
 
     try {
         let result=await UserModal.find();
@@ -36,7 +36,6 @@ userRouter.get("/allusers",async(req,res)=>{
         res.json({"message":"While finding data getting error"})
     }
 })
-
 
 userRouter.get("/alldata/:id",async(req,res)=>{
     const Id=req.params.id;
